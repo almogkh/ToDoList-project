ece4523ad5e843ddcf5e3ba7395475235d785432
Almog Khaikin <almogkh@gmail.com>
2023-05-20 22:52:46 +0300

    Add initial todo list functionality

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,18 @@
 import { type NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
+  //const hello = api.example.hello.useQuery({ text: "from tRPC" });
+  const mut = api.todolist.createTodolist.useMutation();
+  const router = useRouter()
+  const handleCreate = async () => {
+    const id = (await mut.mutateAsync()).id
+    router.push(`/${id}`)
+  }
 
   return (
     <>
@@ -17,20 +24,17 @@ const Home: NextPage = () => {
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
+            ToDo List
           </h1>
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="https://create.t3.gg/en/usage/first-steps"
-              target="_blank"
-            >
-              <h3 className="text-2xl font-bold">First Steps →</h3>
+            <div
+              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20">
+              <h3 className="text-2xl font-bold">Create a new ToDo List</h3>
               <div className="text-lg">
-                Just the basics - Everything you need to know to set up your
-                database and authentication.
+                If you don't have any ToDo lists yet or you want to create a new one, click here:
               </div>
-            </Link>
+              <button onClick={handleCreate} disabled={mut.isLoading}>Create new ToDo List</button>
+            </div>
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
               href="https://create.t3.gg/en/introduction"
@@ -44,7 +48,7 @@ const Home: NextPage = () => {
             </Link>
           </div>
           <p className="text-2xl text-white">
-            {hello.data ? hello.data.greeting : "Loading tRPC query..."}
+            {/*hello.data ? hello.data.greeting : "Loading tRPC query..."*/}
           </p>
         </div>
       </main>
---
+++ src/server/api/routers/todolist.ts
@@ -0,0 +1,55 @@
+import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+export const todolistRouter = createTRPCRouter({
+  createTodolist: publicProcedure
+    .mutation(async ({ ctx }) => {
+      const todolist = await ctx.prisma.todoList.create({data: {}})
+      return {
+        id: todolist.id,
+      };
+    }),
+  getTasks: publicProcedure
+  .input(z.string())
+  .query(({ ctx, input }) => {
+    return ctx.prisma.todoList.findUnique({
+      where: {
+        id: input,
+      },
+      include: {
+        tasks: true,
+      },
+    })
+  }),
+  editTask: publicProcedure
+    .input(
+      z.object({
+        id: z.string(),
+        data: z.object({
+          completed: z.boolean().optional(),
+          description: z.string().optional(),
+          dueDate: z.date().optional(),
+          priority: z.enum(['LOW', 'MEDIUM', 'HIGH']).optional(),
+        }),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const { id, data } = input
+      const todo = await ctx.prisma.task.update({
+        where: { id },
+        data,
+      })
+      return todo
+    }),
+    deleteTask: publicProcedure
+      .input(z.string())
+      .mutation(async ({ ctx, input }) => {
+        const todo = await ctx.prisma.task.delete({
+          where: {
+            id: input
+          }
+        })
+        return todo
+      })
+})
