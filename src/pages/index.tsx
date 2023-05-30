2be898da1807710470e0f54417330e831bd6d311
Almog Khaikin <almogkh@gmail.com>
2023-05-30 03:39:00 +0300

    Update the homescreen

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import { type NextPage } from "next";
 import Head from "next/head";
-import Link from "next/link";
 import { useRouter } from "next/router";
+import { useState } from "react"
 
 import { api } from "~/utils/api";
 
@@ -9,7 +9,10 @@ const Home: NextPage = () => {
   //const hello = api.example.hello.useQuery({ text: "from tRPC" });
   const mut = api.todolist.createTodolist.useMutation();
   const router = useRouter()
+  const [existingId, setExistingId] = useState('')
   const handleCreate = async () => {
+    if (mut.isLoading)
+      return
     const id = (await mut.mutateAsync()).id
     router.push(`/${id}`)
   }
@@ -21,37 +24,46 @@ const Home: NextPage = () => {
         <meta name="description" content="A todo list to manage tasks" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            ToDo List
-          </h1>
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-            <div
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20">
-              <h3 className="text-2xl font-bold">Create a new ToDo List</h3>
-              <div className="text-lg">
-                If you don't have any ToDo lists yet or you want to create a new one, click here:
-              </div>
-              <button onClick={handleCreate} disabled={mut.isLoading}>Create new ToDo List</button>
+      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8 max-w-4xl">
+          <div
+            className="flex flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20 cursor-pointer"
+            onClick={handleCreate}>
+            <h3 className="text-2xl font-bold">Create a new ToDo list</h3>
+            <div className="text-lg">
+              If you don't have any ToDo lists yet or you want to create a new one
             </div>
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="https://create.t3.gg/en/introduction"
-              target="_blank"
-            >
-              <h3 className="text-2xl font-bold">Documentation →</h3>
-              <div className="text-lg">
-                Learn more about Create T3 App, the libraries it uses, and how
-                to deploy it.
-              </div>
-            </Link>
           </div>
-          <p className="text-2xl text-white">
-            {/*hello.data ? hello.data.greeting : "Loading tRPC query..."*/}
-          </p>
+          <div
+            className="flex flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20">
+            <h3 className="text-2xl font-bold">Existing ToDo list</h3>
+            <div className="text-lg flex flex-col">
+              Open an existing ToDo list by opening your saved URL or enter your ToDo list ID<br />
+              <div className="space-x-4 self-center justify-center flex flex-nowrap w-full mt-4">
+                <input
+                  className="w-8/12 text-center text-black"
+                  type="text"
+                  value={existingId}
+                  placeholder="6469157b303eb0c0f8820a32"
+                  onChange={(e) => setExistingId(e.target.value)}
+                  onKeyDown={(e) => {
+                    if (e.key === 'Enter')
+                      router.push(`/${existingId}`)
+                  }} />
+                <button
+                  className="border border-white bg-slate-800 hover:bg-black disabled:bg-slate-800 py-1 px-3"
+                  disabled={existingId === ''}
+                  onClick={() => router.push(`/${existingId}`)}>
+                    Open
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
-      </main>
+        <p className="text-2xl text-white">
+          {/*hello.data ? hello.data.greeting : "Loading tRPC query..."*/}
+        </p>
+      </div>
     </>
   );
 };
